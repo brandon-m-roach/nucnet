e8c094e9a1b8eaf3af6e97ed59351a11a4f120e7
Brandon Michael Roach <broach1@canopus.crc.nd.edu>
2016-10-14 11:03:48 -0400

    removed duplicate bec from flags list in weak_flags_fix.sh

--- weak_flags_fix.sh
+++ weak_flags_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for flag in bec bet+ bet- bkmo btyk bqa+ ec bec bhi+ bex+ mo92 mo03; do
+for flag in bec bet+ bet- bkmo btyk bqa+ ec bhi+ bex+ mo92 mo03; do
     touch $flag.txt;
-    egrep -l "$flag" rapp_rates_unedited/reac*.dat > $flag.txt;
+    egrep -l "$flag" rapp_rates_weak_flags/reac*.dat > $flag.txt;
     #while read file; do 
 #	sed -i -e "${flag} /${flag}w/g"; 
  #   done<${flag}.txt
