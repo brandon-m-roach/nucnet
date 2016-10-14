4515d9ff3c139b6fd6e9d2bc9416e02c26700479
Brandon Michael Roach <broach1@canopus.crc.nd.edu>
2016-10-14 11:10:50 -0400

    minor fixes in weak_flags_fix.sh

--- weak_flags_fix.sh
+++ weak_flags_fix.sh
@@ -3,8 +3,8 @@
 for flag in bec bet+ bet- bkmo btyk bqa+ ec bhi+ bex+ mo92 mo03; do
     touch $flag.txt;
     egrep -l "$flag" rapp_rates_weak_flags/reac*.dat > $flag.txt;
-    #while read file; do 
-#	sed -i -e "${flag} /${flag}w/g"; 
- #   done<${flag}.txt
+    while read file; do 
+	sed -i -e "s/${flag} /${flag}w/g"; 
+    done<${flag}.txt
 done
 	
