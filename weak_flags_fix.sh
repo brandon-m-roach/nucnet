7a170f6eac9499161c838f60d190503b1bb2c7a1
Brandon Michael Roach <broach1@canopus.crc.nd.edu>
2016-10-14 11:12:03 -0400

    added file to sed command

--- weak_flags_fix.sh
+++ weak_flags_fix.sh
@@ -4,7 +4,7 @@ for flag in bec bet+ bet- bkmo btyk bqa+ ec bhi+ bex+ mo92 mo03; do
     touch $flag.txt;
     egrep -l "$flag" rapp_rates_weak_flags/reac*.dat > $flag.txt;
     while read file; do 
-	sed -i -e "s/${flag} /${flag}w/g"; 
+	sed -i -e "s/${flag} /${flag}w/g" $file; 
     done<${flag}.txt
 done
 	
