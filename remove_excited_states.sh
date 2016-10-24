beb63ba2a76479ca53c490df2b431eef49117369
Brandon M. Roach <13roachb@gmail.com>
2016-10-23 20:18:26 -0400

    fixed issues with removing excited states from the xml file and produced the final rapp net

--- remove_excited_states.sh
+++ remove_excited_states.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 #removes files from excited_states.txt from the reaction xml list file
 
+cp rapp_rates_fixed_xml/rapp_rates_fixed_list.xml rapp_rates_fixed_xml/rapp_rates_fixed_no_excited.xml
+
 while read line; do
-    sed "/$line/d" rapp_rates_fixed_xml/rapp_rates_fixed_list.xml > rapp_rates_fixed_xml/rapp_rates_fixed_no_excited.xml;
-    done<excited_states.txt
+    sed -i "/$line/g" rapp_rates_fixed_xml/rapp_rates_fixed_no_excited.xml;
+    done<excited_states_edit.txt
